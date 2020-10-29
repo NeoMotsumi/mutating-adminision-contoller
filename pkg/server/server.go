3a36dd7f7e5f2beb50ccbb82163c858387967a05
Olfodr <neo12motsumi@hotmail.com>
2020-10-29 22:03:27 +0200

    added certs path

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,10 +1,10 @@
 package server
 
 import (
+	"path/filepath"
 	"log"
 	"context"
 	"os/signal"
-	"net"
 	"time"
 	"os"
 	"net/http"
@@ -13,15 +13,30 @@ import (
 //LogFunc ...
 type LogFunc func(msg string, args ...interface{})
 
+const (
+	tlsDir      = `/etc/webhook/certs/`
+)
+
 // NewServer creates a wrapper around the given handler.
-func NewServer(handler http.Handler, timeout time.Duration, signals ...os.Signal) *Server {
+func NewServer(handler http.Handler, timeout time.Duration, addr string, signals ...os.Signal) *Server {
 	s := &Server{}
 	s.server = &http.Server{
 		Handler: handler,
+		Addr: addr,
 	}
 	s.signals = signals
 	s.Log = log.Printf
+
 	return s
+}
+
+
+func (s *Server) ListenAndServeTLS(certFile, keyFile string) {
+	
+	certPath := filepath.Join(tlsDir, certFile)
+	keyPath := filepath.Join(tlsDir, keyFile)
+
+	log.Fatal(s.server.ListenAndServeTLS(certPath, keyPath))
 }
 
 
@@ -45,18 +60,6 @@ func (s *Server) ListenAndServe() error {
 	}()
 	return s.waitForShutdown()
 }
-
-
-// ServeTLS starts a secure http server
-func (s *Server) ServeTLS(l net.Listener, cf, kf string) error {
-	go func() {
-		if err := s.server.ServeTLS(l, cf, kf); err != nil {
-			s.err = err
-		}
-	}()
-	return s.waitForShutdown()
-}
-
 
 func (s *Server) waitForShutdown() error {
 	sig := make(chan os.Signal, 1)
